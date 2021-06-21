1b639ba494f51dbbd889f729bd3a02e99f65ffd6
busyhbrain <82424704+busyhbrain@users.noreply.github.com>
2021-06-21 02:01:22 +0100

    updated

--- validation.js
+++ validation.js
@@ -1,19 +1,38 @@
-const guestList = ["Henry fagbenro","Ahmed oyetoro","Tunde olumuyiwa","Muyiwa johnson","James ayodele", "Clinton fagbenro", "Adetunji fagbenroo"];
 
-//When the validation button is click on the HTML page
- function validation() {
+
+     const guestList = ["Henry fagbenro","Ahmed oyetoro","Tunde olumuyiwa","Muyiwa johnson","James ayodele", "Clinton fagbenro", "Adetunji fagbenroo", "Micheal farounbi"];
+     
+     
+     // alert(guestList);
+    /* function checkName() {
+        fullName = document.getElementById("Name").value;
+        guestList.push(fullName);
+    console.log(guestList) }*/  
+
+     function validation() {
      let name = prompt("What is your Name?");
-//After the name is been inputed in the prompt, it is assumed that they are not in capital letters, so we slice the first letter in the first name to make it capitalised
+    // var name = document.getElementById("Name").type("text");
+    
      let slicedNameA = name.slice(0,1);
      let firstUpperCasedNameA = slicedNameA.toUpperCase();
-//After the first letter of the first name is sliced, we slice the remaining letters of the first name to change them to lowercase
      let slicedNameAA = name.slice(1,name.length);
      let lowerCasedNameAA = slicedNameAA.toLowerCase();
-//The final name after changing the first letter of the first name to uppercase and remaining letters of first name to lowercase is called confirmedName_1
      let confirmedName_1 = firstUpperCasedNameA + lowerCasedNameAA
+     let slicedNameB = name.slice(6,name.length);
+     let slicedNameBB = slicedNameB.slice(0,1)
+     let secondUppercasedNameBB = slicedNameBB.toUpperCase();
+     let slicedNameBBB = slicedNameB.slice(1,name.length);
+     let secondLowercasedNameBB = slicedNameBBB.toLowerCase()
+     let confirmedName_2 = secondUppercasedNameBB + secondLowercasedNameBB
+     let finalNameToConfirm = (confirmedName_1 +" "+ confirmedName_2);
     if (guestList.includes(confirmedName_1)){
-        alert ("Welcome! " + confirmedName_1 )
-    /*  const anser = document.getElementById("Hidden").innerText;    
-      document.getElementById("Hidden").innerText = ( "Welcome! " + confirmedName_1);*/
-     } else alert ("Sorry your name is not on the guest List")
+        // alert ("Welcome! " + confirmedName_1 )
+      let anser = document.getElementById("Welcome").innerText;    
+      document.getElementById("Welcome").innerText = ("Welcome! " + confirmedName_1);
+     } else document.getElementById("Welcome").innerText = ("Sorry your name is not on the guest List Kindly Register")
  }
+
+ 
+ 
+
+//  "Henry fagbenro","Ahmed oyetoro","Tunde olumuyiwa","Muyiwa johnson","James ayodele", "Clinton fagbenro", "Adetunji fagbenroo", "Micheal farounbi"
