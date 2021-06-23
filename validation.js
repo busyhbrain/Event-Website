dd634a38a94afd2ba34e31293d8922fbe15a6bda
busyhbrain <82424704+busyhbrain@users.noreply.github.com>
2021-06-23 18:06:10 +0100

    Add files via upload

--- validation.js
+++ validation.js
@@ -1,6 +1,10 @@
 
 
+<<<<<<< HEAD
      const guestList = ["Henry fagbenro","Ahmed oyetoro","Tunde olumuyiwa","Muyiwa johnson","James ayodele", "Clinton fagbenro", "Adetunji fagbenroo", "Micheal farounbi"];
+=======
+     const guestList = ["Henry fagbenro","Ahmed oyetoro","Tunde olumuyiwa","Muyiwa johnson","James ayodele", "Clinton fagbenro", "Adetunji fagbenroo", "Micheal farounbi"];
+>>>>>>> f9ca9f376727eb79ea49533c67026d252b5f5145
      
      
      // alert(guestList);
@@ -10,6 +14,23 @@
     console.log(guestList) }*/  
 
      function validation() {
+<<<<<<< HEAD
+     var name = prompt("What is your Name?");
+    // var name = document.getElementById("Name").type("text");
+    
+     let slicedNameA = name.slice(0,1);
+     let firstUpperCasedNameA = slicedNameA.toUpperCase();
+     let slicedNameAA = name.slice(1,name.length);
+     let lowerCasedNameAA = slicedNameAA.toLowerCase();
+     let confirmedName_1 = firstUpperCasedNameA + lowerCasedNameAA
+     let slicedNameB = name.slice(6,name.length);
+     let slicedNameBB = slicedNameB.slice(0,1)
+     let secondUppercasedNameBB = slicedNameBB.toUpperCase();
+     let slicedNameBBB = slicedNameB.slice(1,name.length);
+     let secondLowercasedNameBB = slicedNameBBB.toLowerCase()
+     let confirmedName_2 = secondUppercasedNameBB + secondLowercasedNameBB
+     let finalNameToConfirm = (confirmedName_1 +" "+ confirmedName_2);
+=======
      let name = prompt("What is your Name?");
     // var name = document.getElementById("Name").type("text");
     
@@ -25,6 +46,7 @@
      let secondLowercasedNameBB = slicedNameBBB.toLowerCase()
      let confirmedName_2 = secondUppercasedNameBB + secondLowercasedNameBB
      let finalNameToConfirm = (confirmedName_1 +" "+ confirmedName_2);
+>>>>>>> f9ca9f376727eb79ea49533c67026d252b5f5145
     if (guestList.includes(confirmedName_1)){
         // alert ("Welcome! " + confirmedName_1 )
       let anser = document.getElementById("Welcome").innerText;    
@@ -35,4 +57,8 @@
  
  
 
+<<<<<<< HEAD
 //  "Henry fagbenro","Ahmed oyetoro","Tunde olumuyiwa","Muyiwa johnson","James ayodele", "Clinton fagbenro", "Adetunji fagbenroo", "Micheal farounbi"
+=======
+//  "Henry fagbenro","Ahmed oyetoro","Tunde olumuyiwa","Muyiwa johnson","James ayodele", "Clinton fagbenro", "Adetunji fagbenroo", "Micheal farounbi"
+>>>>>>> f9ca9f376727eb79ea49533c67026d252b5f5145
